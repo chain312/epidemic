36e0d817871db125fc1a7ccfe094db196620b72a
chain312 <869553752@qq.com>
2020-02-26 13:52:33 +0800

    2.26日数据更新

--- epidemic/settings.py
+++ epidemic/settings.py
@@ -119,5 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/epidemic/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
